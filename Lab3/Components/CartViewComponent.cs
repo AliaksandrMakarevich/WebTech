c2d0f6f3105949c0554b0e873e36081d71e217b6
Александр Макаревич <alexandrmakarevich1996@gmail.com>
2025-03-20 23:43:18 +0300

    Добавьте файлы проекта.

---
+++ Lab3/Components/CartViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Components
+{
+    public class CartViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
